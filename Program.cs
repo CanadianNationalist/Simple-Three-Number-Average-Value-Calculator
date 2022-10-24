adf1acefaa24964861114aae116c2868f7021eb4
CanadianNationalist <112382954+CanadianNationalist@users.noreply.github.com>
2022-10-24 09:15:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Deklarera det första flyttalet: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Deklarera det andra flyttalet: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Deklarera det tredje flyttalet: ");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Medelvärdet på talet är: " + Medelvärde(a, b, c));
+
+static double Medelvärde(double a, double b, double c)
+{
+return((a+b+c)/3);
+}
